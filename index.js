13a51faddc06ee42b75e45f9a54a4764952f0689
comprefg <39472523+comprefg@users.noreply.github.com>
2023-01-05 23:41:49 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,66 @@
+const Query = require("minecraft-server-util")
+const fs = require('fs');
+const util = require('util')
+
+
+//const domains = [
+  //"mudfish.aternos.host",
+  //"175.107.194.138",
+  //"duckbill.aternos.host",
+  //"barb.aternos.host","jill.aternos.host"
+//];
+
+const fileContents = fs.readFileSync('domains.txt', 'utf8')
+const domains = fileContents.split('\n')
+
+
+var data={};
+try{
+  data = JSON.parse(fs.readFileSync('servers.json'));
+}catch{
+  fs.writeFileSync('servers.json', "{}");
+  data = JSON.parse(fs.readFileSync('servers.json'));
+}
+
+async function pingServer(server,port) {
+  try {
+    sip=server+":"+port
+    
+    const client = require('minecraft-server-util')
+    const mserver = await client.status(server,port)
+    //console.log(sip)
+    //const response = await util.promisify(mserver.ping).call(mserver)
+    //console.log(mserver)
+    //console.log(mserver.players)
+    
+    if (mserver.players.online > -1&&mserver.version.name!="⚠ Error") {
+      data[sip] = {};
+      data[sip]["version"]=mserver.version.name
+      data[sip]["online"]=mserver.players.online
+      data[sip]["players"]=mserver.players.sample
+      console.log(sip);
+      fs.writeFileSync('servers.json', JSON.stringify(data));
+    }
+
+  }
+    //const query = mcserver.status();
+    
+   catch (error) {
+    
+    if(error.message.substring(0, 20)!="Server is offline or"&&error.message.substring(0, 20)!="Socket closed unexpe"&&error.message.substring(0, 20)!="getaddrinfo ENOTFOUN"){
+      console.log(sip)
+      console.log(error);
+      console.log("("+error.message.substring(0, 20)+")");
+    }
+  }
+}
+
+
+  for (const subdomain of domains) {
+    if(subdomain!=""){
+      for (let i = 33710; i < 33712; i++) {
+        setTimeout(() => pingServer(subdomain,i), 0);
+      }
+    }
+}
+
